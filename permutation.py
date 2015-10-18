2e3c0ec3e7188b74309829020741f3bd66571b99
Alexander Black <alexwhb@gmail.com>
2015-10-18 16:44:00 -0700

    a permutation algorithm.

---
+++ permutation.py
@@ -0,0 +1,19 @@
+__author__ = 'alexanderblack'
+
+
+def permutations(string, step = 0):
+    if (step == len(string)):
+        print ''.join(string)
+
+    for i in range(step, len(string)):
+        string_copy = [character for character in string]
+        string_copy[step], string_copy[i] = reverse_char(string_copy[step], string_copy[i])
+        permutations(string_copy, step + 1)
+
+
+def reverse_char(a, b):
+    return b, a
+
+
+if __name__ == '__main__':
+    permutations("abc")
