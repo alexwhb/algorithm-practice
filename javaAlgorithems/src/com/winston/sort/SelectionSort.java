5519a261a13d8bd66dfb5a67e67a2b571368e6b0
Alexander Black <alexwhb@gmail.com>
2015-10-24 11:09:44 -0700

    added selection sort.

---
+++ javaAlgorithems/src/com/winston/sort/SelectionSort.java
@@ -0,0 +1,34 @@
+package com.winston.sort;
+
+public class SelectionSort {
+
+    public static void main(String[] args) {
+        int[] sortableArr = {1, 4, 7, 2, 3, 8 , 12, 16, 3, -5, 2 , 6};
+        selectionSort(sortableArr);
+        for(int item: sortableArr){
+            System.out.println(item);
+        }
+    }
+
+    public static void selectionSort(int[] arr){
+        int i, j;
+        int jMin;
+
+        for(i = 0; i < arr.length -1; i++){
+            jMin = i;
+            for(j = i+1; j < arr.length; j++){
+                if(arr[j] < arr[jMin]){
+                    jMin = j;
+                }
+            }
+            
+            if(jMin != i){
+                //swap numbers
+               int temp =  arr[i];
+               arr[i] = arr[jMin];
+               arr[jMin] = temp;
+            }
+        }
+    }
+
+}
