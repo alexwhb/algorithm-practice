206362c346cedd2b19d246db8d37dd66d7612488
Alexander Black <alexwhb@gmail.com>
2015-11-05 21:07:21 -0800

    added merge sort implementation.

---
+++ javaAlgorithems/src/com/winston/sort/MergeSort.java
@@ -0,0 +1,54 @@
+package com.winston.sort;
+
+public class MergeSort {
+
+
+    public static void main(String[] args) {
+        Integer[] sortableArr = {1, 4, 7, 2, 3, 8 , 12, 16, 3, -5, 2 , 6};
+        mergeSort(sortableArr);
+        for(int item: sortableArr){
+            System.out.println(item);
+        }
+    }
+
+    public static void mergeSort(Comparable [ ] a)
+    {
+        Comparable[] tmp = new Comparable[a.length];
+        mergeSort(a, tmp,  0,  a.length - 1);
+    }
+
+    private static void mergeSort(Comparable [ ] a, Comparable [ ] tmp, int left, int right)
+    {
+        if( left < right )
+        {
+            int center = (left + right) / 2;
+            mergeSort(a, tmp, left, center);
+            mergeSort(a, tmp, center + 1, right);
+            merge(a, tmp, left, center + 1, right);
+        }
+    }
+
+    private static void merge(Comparable[ ] a, Comparable[ ] tmp, int left, int right, int rightEnd )
+    {
+        int leftEnd = right - 1;
+        int k = left;
+        int num = rightEnd - left + 1;
+
+        while(left <= leftEnd && right <= rightEnd)
+            if(a[left].compareTo(a[right]) <= 0)
+                tmp[k++] = a[left++];
+            else
+                tmp[k++] = a[right++];
+
+        while(left <= leftEnd)    // Copy rest of first half
+            tmp[k++] = a[left++];
+
+        while(right <= rightEnd)  // Copy rest of right half
+            tmp[k++] = a[right++];
+
+        // Copy tmp back
+        for(int i = 0; i < num; i++, rightEnd--)
+            a[rightEnd] = tmp[rightEnd];
+    }
+
+}
